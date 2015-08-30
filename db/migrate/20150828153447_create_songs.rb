0e61ff55a523460cb5634071a7a408c117015b10
Kenneth Liu & Nicholas Kincaid <pair+ksliu25.nbkincaid@devbootcamp.com>
2015-08-30 18:04:35 -0500

    add tag-lib meta-tag funcionality

--- db/migrate/20150828153447_create_songs.rb
+++ db/migrate/20150828153447_create_songs.rb
@@ -1,13 +1,16 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :original_filepath, null: false
-      t.string :original_filename, null: false
-      t.string :s3_url, null: false
-      t.string :s3_filename, null: false
-      t.string :artist
-      t.string :album
-      t.string :song_name
+      t.string  :original_filepath, null: false
+      t.string  :original_filename, null: false
+      t.string  :s3_url, null: false
+      t.string  :s3_filename, null: false
+      t.string  :artist
+      t.string  :album
+      t.integer :track
+      t.string  :title
+      t.string  :genre
+      t.integer :release_year
       t.integer :owner_id, null: false
 
       t.timestamps null: false
