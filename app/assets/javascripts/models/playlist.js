323dc317d885b55246db19ac3d958f4cc9a15ce6
Kenneth Liu & Nicholas Kincaid <pair+ksliu25.nbkincaid@devbootcamp.com>
2015-08-31 16:19:07 -0500

    updated mvc version of show currently playing

--- app/assets/javascripts/models/playlist.js
+++ app/assets/javascripts/models/playlist.js
@@ -1,12 +1,4 @@
 var Playlist = function(){
-    // this.currentURL = null;
-    // this.currentIndex = null;
-    // this.currentTitle = null;
-    // this.currentArtist = null;
-    // this.currentAlbum = null;
-    // this.currentTrack = null;
-    // this.currentGenre = null;
-
     this.songs = [];
     this.currentSong = null;
 };
@@ -36,13 +28,11 @@ Playlist.prototype = {
     function(event){
       event.preventDefault();
 
-      // identify the row that has the song details
+
       var row = $(event.target).parent().parent();
 
-      // remove the "playing" from existing current played song
       $('.playing').removeClass('playing')
 
-      // assign the "active" class to the row that was clicked
       row.addClass('playing');
 
       // // find the data in the row of the link that was clicked
