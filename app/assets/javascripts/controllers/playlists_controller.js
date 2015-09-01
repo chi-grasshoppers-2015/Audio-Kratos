86349cd0b3e4775b6e9626bc7387881486a296ff
Spencer Rohan <spencermrohan@gmail.com>
2015-09-01 18:36:39 -0500

    no comment

--- app/assets/javascripts/controllers/playlists_controller.js
+++ app/assets/javascripts/controllers/playlists_controller.js
@@ -2,9 +2,6 @@ var PlaylistsController = function(model, view){
   this.plModel= model;
   this.plView = view;
 
-  console.log("hey!");
-  console.log(this.plModel);
-  console.log(this.plView)
 };
 
 PlaylistsController.prototype = {
@@ -21,13 +18,9 @@ PlaylistsController.prototype = {
   columnSort:
     function(event){
       event.preventDefault();
-      console.log(this.plModel.songs);
-
       var sortKey = $(event.target).html();
       var saniSortKey = sortKey.toLowerCase();
-      console.log(saniSortKey);
       this.plModel.songs.sort(this.compareOnKey(saniSortKey))
-      console.log(this.plModel.songs);
       this.plView.updateTable(this.plModel.songs);
 
       this.plView.updatePlayingRow(this.plModel.currentSongIndex());
