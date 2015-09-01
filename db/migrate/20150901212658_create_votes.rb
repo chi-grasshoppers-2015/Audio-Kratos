3ea39f9a80cdde414b72169a4b49728fb7b2b458
Nicholas Kincaid <nbkincaid@gmail.com>
2015-09-01 16:30:42 -0500

    added votes model migration

---
+++ db/migrate/20150901212658_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :song_id, null: false
+      t.integer :event_id, null: false
+      t.integer :user_id, null: false
+      t.integer :value, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
