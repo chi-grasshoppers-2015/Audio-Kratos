186bb7e608adc3be777be481d0608eabf39631f1
Nicholas Kincaid <nbkincaid@gmail.com>
2015-09-01 13:20:12 -0500

    add events index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :songs, foreign_key: "owner_id"
   has_many :playlists, foreign_key: "owner_id"
+  has_many :events, foreign_key: "owner_id"
 
   validates_presence_of :first_name, :last_name, :email, :hashed_password
   validates :email, uniqueness: true
