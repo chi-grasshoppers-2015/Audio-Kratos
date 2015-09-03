11b5331b884236c55e86cce40c84ce50c6754b78
Kenneth Liu <ksliu25@gmail.com>
2015-09-03 13:43:17 -0500

    fix precompile error on init guest in events_controller.js

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
-Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-ALBUMART = AmazonAlbumArt.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], 'bytewayve-20')
+# Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+# ALBUMART = AmazonAlbumArt.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], 'bytewayve-20')
 
 
 
