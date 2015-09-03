4bd30348199e855ba6b9d78fc9abc8f9fd28912a
Kenneth Liu <ksliu25@gmail.com>
2015-09-03 13:49:44 -0500

    fix aws initializer, remove comments

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,2 @@
 Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 ALBUMART = AmazonAlbumArt.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], 'bytewayve-20')
-
-
-
-Aws.config(
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-)
