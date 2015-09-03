3c73467ff41eaff6dffc02ced8d12a13d19e8151
Kenneth Liu <ksliu25@gmail.com>
2015-09-03 11:23:34 -0500

    add edit to clear out the votes for all songs on a playlist when added to an event

--- app/controllers/playlist_events_controller.rb
+++ app/controllers/playlist_events_controller.rb
@@ -2,9 +2,14 @@ class PlaylistEventsController < ApplicationController
 
   def create
     @event = Event.find(params[:event_id])
-    @playlistevent = PlaylistEvent.new(playlist_id: params[:playlist_event][:playlist_id], event_id: params[:event_id])
+
+    @playlist = Playlist.find(params[:playlist_event][:playlist_id])
+    @playlistevent = PlaylistEvent.new(playlist_id: @playlist.id, event_id: @event.id)
 
     if @playlistevent.save
+      @playlist.songs.each { |song| song.votes.each {|vote| vote.destroy}}
+      @playlist.songs.each { |song| song.update_attributes(net_votes: 0)}
+   
       redirect_to @event
     else
       redirect_to @event
